2634c95ef4acd6627871208e73dd489f12a5fa43
anavilla <a.villanueva.2018@>
2022-02-21 16:56:10 +0100

    Mensaje

--- P1/tienda.js
+++ P1/tienda.js
@@ -6,22 +6,6 @@ const url = require('url');
 //-- Puerto -->
 const PUERTO = 9090;
 
-//-- Texto HTML de la página de error -->
-const pagina_error = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi tienda</title>
-</head>
-<body style="background-color: red">
-    <h1 style="color: white">ERROR!!!!</h1>
-</body>
-</html>
-`
-
 //-- Servidor -->
 console.log("Escuchando...");
 
@@ -39,15 +23,19 @@ const server = http.createServer((req, res) => {
   fs.readFile(filename, (err, data) => {
     if (err) {
         console.log('Error')
-        const data = fs.readFileSync('error.html','utf8');
-        //res.statusCode = 404;
-        //res.statusMessage = "Ha habido un error";
-        //res.setHeader('Content-Type', "text/html");
+        data = fs.readFileSync('error.html','utf8');
+        res.statusCode = 404;
+        res.statusMessage = "Not Found";
+        res.setHeader('Content-Type', "text/html");
         res.write(data);
         return res.end();
     }
 
     console.log(content_type)
+    let code = 200;
+    let code_msg = "OK";
+    res.statusCode = code;
+    res.statusMessage = code_msg;
     res.write(data);  //Su ausencia da lugar a error 
     res.end();      //Su ausencia da lugar a error 
   });  
