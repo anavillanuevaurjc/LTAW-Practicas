eb004d0083b6e3c32fed412b5e9828c4e46e71b4
anavilla <a.villanueva.2018@>
2022-02-28 12:31:11 +0100

    bien

--- P2/S05/E3.js
+++ P2/S05/E3.js
@@ -4,7 +4,7 @@
 const fs = require('fs'); //Acceso sistema de ficheros 
 
 //-- Npmbre del fichero JSON a leer
-const FICHERO_JSON = "Ej-03-tienda-json-fich.json" //Nombre fichero JSON usado 
+const FICHERO_JSON = "E3.json" //Nombre fichero JSON usado 
 
 //-- Leer el fichero JSON 
 const  tienda_json = fs.readFileSync(FICHERO_JSON); //Lectura sincrona (en asignatura uso de lecturas asincronas)
