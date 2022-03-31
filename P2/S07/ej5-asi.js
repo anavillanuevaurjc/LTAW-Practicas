580ebc7787db16b79ffe101e0b5e6c77345d1a39
anavilla <a.villanueva.2018@>
2022-03-31 11:31:26 +0200

    busquedas asin falla

--- P2/S07/ej5-asi.js
+++ P2/S07/ej5-asi.js
@@ -10,6 +10,8 @@ const MAIN = fs.readFileSync('Ej-01.html','utf-8');
 //-- Leer fichero JSON con los productos
 const PRODUCTOS_JSON = fs.readFileSync('Ej-01.json');
 
+let productos = JSON.parse(PRODUCTOS_JSON);
+
 //-- SERVIDOR: Bucle principal de atención a clientes
 const server = http.createServer((req, res) => {
 
@@ -19,7 +21,7 @@ const server = http.createServer((req, res) => {
     console.log(myURL.pathname);
     //-- Por defecto entregar página web principal
     if (myURL.pathname == "/"){
-        filename = "Ej-02.html";
+        filename = "Ej-05.html";
         content = (myURL.pathname).split(["."])[1]
         if (content == "jpg" || content == "JPG") {
           content_type = "image/" + "jpeg";
@@ -36,14 +38,42 @@ const server = http.createServer((req, res) => {
     if (myURL.pathname == '/productos') {
         content_type = "application/json";
         filename = 'Ej-01.json';
+        
     }
 
-    if (myURL.pathname == '/cliente-1.js') {
+    if (myURL.pathname == '/cliente-4.js') {
       content_type = "application/javascript";
-      filename = 'cliente-1.js';
+      filename = 'cliente-4.js';
   }
   
     fs.readFile(filename, (err, data) => {
+      
+      if (filename == '/productos'){
+        let param1 = myURL.searchParams.get('param1');
+
+        param1 = param1.toUpperCase();
+
+        console.log("  Param: " +  param1);
+
+        let result = [];
+
+        for (let prod of productos) {
+
+          //-- Pasar a mayúsculas
+          prodU = prod.toUpperCase();
+
+          //-- Si el producto comienza por lo indicado en el parametro
+          //-- meter este producto en el array de resultados
+          if (prodU.startsWith(param1)) {
+              result.push(prod);
+          }
+          
+      }
+      console.log(result + "REEEEEEESIL");
+      data = JSON.stringify(result);
+      }
+
+      console.log(filename + "aaaaaaaaaaa");
         if (err) {
             console.log('Error')
             let code = 404;
