cd0ec0c8d8fea2aa40ac8fef5886de97d0282bd5
anavilla <a.villanueva.2018@>
2022-05-03 10:13:25 +0200

    no funciona

--- P4/index.js
+++ P4/index.js
@@ -2,12 +2,12 @@
 const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
 
+const btn_test = document.getElementById("btn_test");
+
 const info1 = document.getElementById("info1");
 const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
 
-//-- Crear un websocket. Se establece la conexión con el servidor
-//const socket = io();
 
 //-- Acceder a la API de node para obtener la info
 //-- Sólo es posible si nos han dado permisos desde
@@ -15,6 +15,9 @@ const info3 = document.getElementById("info3");
 info1.textContent = process.versions.node;
 info2.textContent = process.versions.chrome;
 info3.textContent = process.versions.electron;
+
+//-- Crear un websocket. Se establece la conexión con el servidor
+const socket = io();
 
 //--Acciones en función del mensaje recibido
 socket.on("message", (msg)=>{
@@ -28,4 +31,11 @@ msg_entry.onchange = () => {
   
   //-- Borrar el mensaje actual
   msg_entry.value = "";
+}
+
+btn_test.onclick = () => {
+  console.log("Botón apretado!");
+
+  //-- Enviar mensaje al proceso principal
+  electron.ipcRenderer.invoke('test', "MENSAJE DE PRUEBA: Que tengas un bonito día :D");
 }
