c829b0ebb99333f0821ce8e334b25782ae4e8994
anavilla <a.villanueva.2018@>
2022-05-03 10:51:51 +0200

    funciona mensaje

--- P4/index.js
+++ P4/index.js
@@ -1,13 +1,14 @@
-//-- Elementos del interfaz
-const display = document.getElementById("display");
-const msg_entry = document.getElementById("msg_entry");
+const electron = require('electron');
 
+console.log("Hola desde el proceso de la web...");
+
+//-- Obtener elementos de la interfaz
 const btn_test = document.getElementById("btn_test");
-
+const display = document.getElementById("display");
 const info1 = document.getElementById("info1");
 const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
-
+const print = document.getElementById("print");
 
 //-- Acceder a la API de node para obtener la info
 //-- Sólo es posible si nos han dado permisos desde
@@ -16,31 +17,16 @@ info1.textContent = process.versions.node;
 info2.textContent = process.versions.chrome;
 info3.textContent = process.versions.electron;
 
-//-- Crear un websocket. Se establece la conexión con el servidor
-const socket = io();
-
-//--Acciones en función del mensaje recibido
-socket.on("message", (msg)=>{
-    display.innerHTML = '<p style="color:black">' + msg + '</p>' + display.innerHTML;
-});
-
-//-- Al apretar el botón se envía un mensaje al servidor
-msg_entry.onchange = () => {
-  if (msg_entry.value)
-    socket.send(msg_entry.value);
-  
-  //-- Borrar el mensaje actual
-  msg_entry.value = "";
-}
-
 btn_test.onclick = () => {
-  console.log("Botón apretado!");
+    display.innerHTML += "MENSAJE DE PRUEBA: Hola, chato! ";
+    console.log("Botón apretado!");
 
-  //-- Enviar mensaje al proceso principal
-  electron.ipcRenderer.invoke('test', "MENSAJE DE PRUEBA: Boton apretado");
+    //-- Enviar mensaje al proceso principal
+    electron.ipcRenderer.invoke('test', "MENSAJE DE PRUEBA: Boton apretado");
 }
+
 //-- Mensaje recibido del proceso MAIN
 electron.ipcRenderer.on('print', (event, message) => {
-  console.log("Recibido: " + message);
-  print.textContent = message;
-});
+    console.log("Recibido: " + message);
+    print.textContent = message;
+  });
