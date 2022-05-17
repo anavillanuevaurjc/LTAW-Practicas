f3cbfd5be085ac1a2394a5b316b6478d36898d21
anavilla <a.villanueva.2018@>
2022-05-17 10:47:00 +0200

    no tengo claro que suene - vnc

--- P3/chat.js
+++ P3/chat.js
@@ -2,6 +2,7 @@
 const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
 const msg_nick = document.getElementById("msg_nick");
+const sonido = new Audio("/public/P3_rebote.mp3");
 
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
@@ -9,11 +10,12 @@ const socket = io();
 //--Acciones en función del mensaje recibido
 socket.on("message", (msg)=>{
     display.innerHTML = '<p style="color:black">' + msg + '</p>' + display.innerHTML;
-});
+  });
 //-- Al apretar el botón se envía un mensaje al servidor
 msg_nick.onchange = () => {
   if (msg_nick.value)
   socket.send("Nick = " + msg_nick.value);
+  sonido.play();
 }
 
 
@@ -24,4 +26,5 @@ msg_entry.onchange = () => {
   
   //-- Borrar el mensaje actual
   msg_entry.value = "";
+  sonido.play();
 }
